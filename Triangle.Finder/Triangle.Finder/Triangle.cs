e1ef27249302d9031205a1f4c299ec2532f063ed
Khang Yeen Lee <leekhangyeen@gmail.com>
2016-12-03 14:15:24 +0100

    Added C# project. Easier for me to finish the project.

---
+++ Triangle.Finder/Triangle.Finder/Triangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle.Finder
+{
+    /*
+     * Parent Triangle class
+     */
+    public abstract class Triangle
+    {
+        protected List<int> sides;
+
+        public Triangle()
+        {
+        }
+
+    }
+
+    public class Equilateral : Triangle
+    {
+
+        public Equilateral()
+            : base()
+        {
+        }
+    }
+
+    public class Isosceles : Triangle
+    {
+    }
+
+    public class Scalene : Triangle
+    {
+    }
+}
